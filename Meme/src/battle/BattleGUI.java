a90a4df3d6704d0bd5416127c0e280a6d6b947cb
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2018-01-12 10:47:00 -0500

    BattleGUI

---
+++ Meme/src/battle/BattleGUI.java
@@ -0,0 +1,26 @@
+package battle;
+
+import guiTeacher.GUIApplication;
+
+public class BattleGUI extends GUIApplication {
+	public static BattleGUI gui;
+	public static BattleScreen battleScreen;
+	
+	public BattleGUI(int width, int height) {
+		super(width, height);
+		setVisible(true);
+	}
+
+	@Override
+	public void initScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		gui = new BattleGUI(800,800);
+		Thread go = new Thread(gui);
+		go.start();
+	}
+
+}
