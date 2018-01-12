77c501348ebfad0742a03225831ad812a6508641
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2018-01-12 10:22:22 -0500

    screen

--- Meme/src/battle/BattleScreen.java
+++ Meme/src/battle/BattleScreen.java
@@ -11,18 +11,13 @@ public class BattleScreen extends FullFunctionScreen {
 	public static BattleScreen battle;
 	public BattleScreen(int width, int height) {
 		super(width, height);
-		setVisible(true);
+		
 	}
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
 		// TODO Auto-generated method stub
 		
-	}
-	public static void main(String[] args) {
-		 battle= new BattleScreen(800, 500);
-		Thread runner = new Thread(battle);
-		runner.start();
 	}
 
 }
