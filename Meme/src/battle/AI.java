446793b670b04ce691079d12b287ea1b7e3c0452
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2018-01-16 10:42:18 -0500

    AI class

--- Meme/src/battle/AI.java
+++ Meme/src/battle/AI.java
@@ -1,5 +1,29 @@
 package battle;
 
-public interface AI {
+import java.util.ArrayList;
 
+public class AI {
+	public ArrayList<Card> deck;
+	public ArrayList<Card> hand;
+	public int health;
+	public ArrayList<Card> board;
+	
+	public Card[] getBoard() {
+		return board;
+	}
+	
+	public int changeHealth(int change) {
+		health += change;
+	}
+	
+	public void draw() {
+		if(deck.size() == 0) {
+			lose();
+		}
+	}
+
+	private void lose() {
+		// TODO Auto-generated method stub
+		
+	}
 }
