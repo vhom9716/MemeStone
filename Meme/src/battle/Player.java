0181a7360c29d5c0c1ac6a7ca051603a77e1716f
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2018-01-17 10:46:49 -0500

    ZSAD

--- Meme/src/battle/Player.java
+++ Meme/src/battle/Player.java
@@ -8,19 +8,43 @@ public class Player {
 	public ArrayList<Card> deck;
 	public ArrayList<Card> hand;
 	public ArrayList<Card> board;
+	public boolean[] playable; 
 	public int health;
 	public int currentmana;
 	public int maxmana;
 	
 	public Player() {
-		maxmana = 0;
+		maxmana = 10;
 		health = 30;
 		currentmana = 0;
+		sendinfortofront();
+	}
+	private boolean[] sendinfortofront() {
+		playable=new boolean[hand.size()];
+		for(int i=0;i<hand.size();i++) {
+			if(hand.get(i).mana>currentmana) {
+				playable[i]=false;
+			}else {
+				playable[i]=true;
+			}
+		}
+		return playable;
+		
 	}
 	public void drawcard() {
 		Card x = deck.get(0);
 		deck.remove(0);
 		hand.add(x);
+	}
+	public void changehp(int change) {
+		health+=change;
+	}
+	public void playcard(Card x) {
+		currentmana-=x.mana;
+		board.add(x);
+		hand.remove(x);
+		sendinfortofront();
+		
 	}
 	
 	public void declareAttack(Card attacker,Card victim) {
