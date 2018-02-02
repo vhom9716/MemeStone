ea1c0915a0c601bacd9d93831970a67df478f846
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2018-02-02 10:12:14 -0500

    a

--- Meme/src/battle/Player.java
+++ Meme/src/battle/Player.java
@@ -5,24 +5,30 @@ import java.util.ArrayList;
 import cards.Card;
 
 public class Player {
+	
 	public ArrayList<Card> deck;
 	public ArrayList<Card> hand;
 	public ArrayList<Card> board;
 	public boolean[] playable; 
-	public int health;
-	public int currentmana;
-	public int maxmana;
+	public int health=30;
+	public int currentmana=0;
+	public int manaslot=0;
+	public int maxmana=10;
+	private String name;
 	
-	public Player() {
-		maxmana = 10;
-		health = 30;
-		currentmana = 0;
+	public Player(String name,int health,int maxmana,int mana,ArrayList<Card> deck, ArrayList<Card> hand) {
+		this.name=name;
+		this.health=health;
+		this.deck=deck;
+		this.hand=hand;
+		this.currentmana=mana;
+		this.maxmana=maxmana;
 		sendinfortofront();
 	}
 	private boolean[] sendinfortofront() {
 		playable=new boolean[hand.size()];
 		for(int i=0;i<hand.size();i++) {
-			if(hand.get(i).mana>currentmana) {
+			if(hand.get(i).getCost()>currentmana) {
 				playable[i]=false;
 			}else {
 				playable[i]=true;
@@ -31,13 +37,52 @@ public class Player {
 		return playable;
 		
 	}
-	public void drawcard() {
-		Card x = deck.get(0);
-		deck.remove(0);
-		hand.add(x);
+	public void drawcard(int amount) {
+		if(decksize()==0) {
+			System.out.println("out of cards");
+			health--;
+		}else {
+			while(amount<=0) {
+				Card x = deck.get(0);
+				deck.remove(0);
+				hand.add(x);
+				checkhand();
+				amount--;
+			}
+		}
+		
+		
 	}
-	public void changehp(int change) {
-		health+=change;
+	public int decksize() {
+		return deck.size();
+	}
+	private void checkhand() {
+		if(hand.size()>4) {
+			hand.remove(3);
+		}
+		
+	}
+	public void givemanaslot() {
+		if(manaslot<maxmana) {
+			manaslot++;
+		}
+	}
+	public void refillMana() {
+		currentmana=manaslot;
+	}
+	public void heal(int heal) {
+		if(health+heal>30) {
+			health=30;
+		}else {
+			health+=heal;
+		}
+		
+	}
+	public void takedamage(int damage) {
+		health-=damage;
+	}
+	public boolean canplaycard() {
+		return hand.stream().filter(card->card.getCost()<=currentmana).count() >0;
 	}
 	public int returnhp() {
 		return health;
