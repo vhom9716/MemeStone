7d13b1b3fc3076f49a058cff480529f869c4255a
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2018-02-06 11:11:59 -0500

    a

--- Meme/src/menu/ShopScreen.java
+++ Meme/src/menu/ShopScreen.java
@@ -3,6 +3,7 @@ package menu;
 import java.awt.Color;
 import java.util.List;
 
+import cards.Card;
 import guiTeacher.components.*;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
@@ -35,20 +36,22 @@ public class ShopScreen extends FullFunctionScreen {
 			@Override
 			public void act() {
 				int getCard;
-				if(gold >= 100) {
-					 = (int) (Math.random()*100);
-					if(getCards > 95) {
-						
-					}
+				if (gold >= 100){
+					gold = (int) (Math.random()*100);
+//					if(getCards > 95) {
+//						
+//					}
 				}
 				//Do stuff so you get 5 cards.
 				//Animate the cards so they all are revealed at the same time.
 			}
 		});
 		viewObjects.add(buyPack);
-		TextLabel displayGold = new TextLabel(0, 0,200,200,"AAAAAAAAAAAA");
-		displayGold.setCustomTextColor(Color.RED);
-		displayGold.setText("LOL");
+		TextLabel displayGold = new TextLabel(383, 15,200,200,Integer.toString(gold));
+		displayGold.setCustomTextColor(Color.WHITE);
+		displayGold.setSize(29
+				);
+		displayGold.setText(Integer.toString(gold));
 		viewObjects.add(displayGold);
 	}
 	
