e28ec5382c84e631451d38a69ad4ba7868f30842
JasonYan <jyan7555@bths.edu>
2018-03-01 22:22:13 -0500

    !

--- Meme/src/battle/BattleScreen.java
+++ Meme/src/battle/BattleScreen.java
@@ -367,10 +367,12 @@ import menu.Menu;
 			}
 		}
 		if (field == AIfieldSlots) {
-			selectedPosC = pos;
-			enemySelected = true;
-			enemyFighter = backend.computerBoard.get(pos);
-			System.out.println("Enemy chossen");
+			if(friendlySelected) {
+				selectedPosC = pos;
+				enemySelected = true;
+				enemyFighter = backend.computerBoard.get(pos);
+				System.out.println("Enemy chossen");
+			}
 		}
 		if (friendlySelected==true && enemySelected==true) {
 			System.out.println("myside"+backend.playerBoard.size());
